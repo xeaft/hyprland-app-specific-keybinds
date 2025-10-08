ed5c3f56d86e4303fd8249071366f5dc2c57d51c
xeaft <main@kvejp.dev>
2025-10-08 17:09:50 +0200

    write help for stop/reload keywords

--- pysrc/main.py
+++ pysrc/main.py
@@ -1,14 +1,19 @@
+import sys
 import socket_listener
 import conf_parser
 import argparse
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog="hyprwinbinds", description="allows you to set keybinds for specific window classes ")
+    parser = argparse.ArgumentParser(prog="hyprwinbinds", description="allows you to set keybinds for specific window classes", usage="%(prog)s [keyword?] [options?]")
     
     parser.add_argument("-l", "--show-logs", action="store_true")
     parser.add_help = True
     
     args = parser.parse_args()
+
+    if args.reload or args.stop:
+        print("use the wrapper script for reload/stop")
+        sys.exit()
 
     config = conf_parser.read_keybinds_file()
     keys = conf_parser.parse_key_lines(config)
